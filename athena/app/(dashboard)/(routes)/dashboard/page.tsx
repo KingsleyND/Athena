a2d6a3f664f198fd7da5c726b0527080f05458f4
KingsleyND <72080248+KingsleyND@users.noreply.github.com>
2025-05-20 20:57:38 -0400

    initial pages and components

---
+++ athena/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+  return( 
+    <div>
+  <h1 className="text-6xl text-orange-500">Dashboard Page(protected)</h1>
+
+
+</div>
+
+  )
+}
---
+++ athena/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+    return ( 
+        <div>
+            Landing Page(not protected)
+            <p className="text-6xl text-teal-500">Athena</p>
+        </div>
+     );
+}
+ 
+export default LandingPage;
